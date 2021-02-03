2f52184283a77b3ddaa66eceabf9026d1a26fb9e
Alvin Wong <wong.alvin13@gmail.com>
2021-02-03 13:28:02 -0600

    postman

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const quotes = require('./modules/quotes');
 //console.log('express', express);
 
 // Create our app
@@ -10,6 +11,9 @@ const port = 3000;
 // Share any files inside the "public" folder
 app.use(express.static('server/public'));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Listen for network requests
 app.listen(port, function () {
   // When the server is ready, call this function
@@ -20,3 +24,27 @@ app.listen(port, function () {
   // killall -9 node
   // try again after
 });
+
+app.get('/quotes', function (req, res) {
+  console.log('get quotes');
+  res.send(quotes.getNextQuote());
+});
+
+app.get('/quotes', (req, res) => {
+  let quote = req.body.quote_to_add;
+  console.log(quote.author);
+  console.log(quote.quote);
+
+  // add to quoteData
+  quotes.addQuote(quote);
+  // respond with ok
+  res.sendStatus(200);
+});
+
+// Postman
+// {
+//   "quote_to_add": {
+//     "quote": "You found the marble in the oatmeal!",
+//     "author": "Stanley Tudexci"
+//   }
+// }
